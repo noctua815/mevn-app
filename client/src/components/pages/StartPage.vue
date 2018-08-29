fb786320ab6131d294f4f1956245f0974866d54b
Vika Denisova <vika.denisova@lebedev-studio.com>
2018-08-29 18:05:03 +0300

    1 part

---
+++ client/src/components/pages/StartPage.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  .container
+    .row
+      .col-xs-12
+        h1
+          | Main page
+</template>
+
+<script>
+export default {
+  name: 'StartPage',
+};
+</script>
---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const config = require('./config/config');
+
+const app = express();
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(process.env.PORT || config.port,
+  () => console.log(`Server start on port ${config.port} ...`));
+
+app.get('/posts', (req, res) => {
+  res.send(
+    [{
+      title: 'Hello World!',
+      description: 'Hi there! How are you?',
+    }],
+  );
+});
