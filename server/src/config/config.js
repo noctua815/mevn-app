471d0387c4119675980a77f1ec3ad8f680ad9697
Vika Denisova <vika.denisova@lebedev-studio.com>
2018-09-03 17:20:06 +0300

    Drop all

--- server/src/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  port: 8081,
-  dbURL: 'mongodb://localhost/articles',
-  // dbOptions: {},
-  dbOptions: { useNewUrlParser: true },
-  // dbOptions: { useMongoClient: true },
-};
